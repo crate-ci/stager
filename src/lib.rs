4ec9d52f758dbe4a7a9fb09ea76a4f0eaa0a781a
Ed Page <eopage@byu.net>
2018-04-08 21:23:27 -0600

    feat(de): Template support
    Most fields in the deserialization format now support Liquid
    expressions.
    
    As part of this, `de` has been made into a default feature so users can
    turn it off if desired.
    
    Fixes #1 String Template Support
    Fixex #8 Data Directory Support
    
    BREAKING CHANGES:
    - Paths with `{` or `}` might end up being treated as liquid templates.
      Later this can be worked around when the `raw` tag is supported.
    - API changes to make the above possible.

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,13 @@
 #[macro_use]
 extern crate failure;
 extern crate globwalk;
+#[cfg(feature = "de")]
+extern crate liquid;
+#[cfg(feature = "de")]
 #[macro_use]
 extern crate serde;
 
 pub mod action;
 pub mod builder;
+#[cfg(feature = "de")]
 pub mod de;
